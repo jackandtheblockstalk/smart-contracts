44eb202667ec05e83dcd6c824b33cc884c0542d0
Jack Tanner <ugmo04@hotmail.com>
2018-01-27 15:30:48 +0700

    Update VulnerableLottery.sol

--- blockchainconnector/VulnerableLottery.sol
+++ blockchainconnector/VulnerableLottery.sol
@@ -1,13 +1,15 @@
 pragma solidity ^0.4.4;
+
 // WARNING THIS CODE IS AWFUL, NEVER DO ANYTHING LIKE THIS
+
 contract Oracle{
 	uint8 seed;
-	function Oracle(uint8 _seed){ // SEED VALUE CAN BE OBTAINED BY LOOKING AT FUNCTION CALL
+	function Oracle(uint8 _seed){ // 1. SEED VALUE CAN BE OBTAINED BY LOOKING AT FUNCTION CALL
 		seed = _seed;
 	}
 
 	function getRandomNumber() external returns (uint256){
-		return block.number % seed; // THIS CAN BE FIGURED OUT, NOT SECURE
+		return block.number % seed; // 2. THIS CAN BE FIGURED OUT, NOT SECURE
 	}
 }
 
@@ -21,7 +23,7 @@ contract Lottery{
     mapping(address=>uint256) public balances;
     mapping (address=>string) public teamNames;
     address [] public teams;
-    string [] private passwords; // THERE IS NO VALUE IN THIS VARIABLE
+    string [] private passwords; // 3. THERE IS NO VALUE IN THIS VARIABLE
 
 
     event TeamRegistered(string name);
@@ -43,7 +45,7 @@ contract Lottery{
 
     // initialise the oracle and lottery end time
     function initialiseLottery(uint8 seed) onlyowner {
-    	oracle = new Oracle(seed);  // INSECURE, SEED CAN BE OBTAINED
+    	oracle = new Oracle(seed);  // 1. INSECURE, SEED CAN BE OBTAINED
     	endTime = now + 7 days;
     	teams.push(0x0);
     	teamNames[0x0] = "Default Team";
@@ -53,16 +55,17 @@ contract Lottery{
     // reset the lottery
     function reset(uint8 _newSeed) onlyowner {
     	endTime = now + 7 days;
-        resetOracle(_newSeed); // INSECURE, SEED CAN BE OBTAINED
+        resetOracle(_newSeed); // 4. INSECURE, SEED CAN BE OBTAINED
     }
 
     // register a team
-    function registerTeam(address _walletAddress,string _teamName, string _password){ // USE OF _walletAddress WITH  NO SENDER CHECKS MEANS ANYONE CAN OVERWRITE ANOTHER
+    function registerTeam(address _walletAddress, string _teamName, string _password){
+    	// 5. USE OF _walletAddress WITH  NO SENDER CHECKS MEANS ANYONE CAN OVERWRITE ANOTHER
     	teams.push(_walletAddress);
     	passwords.push(_password);
     	teamNames[_walletAddress] = _teamName;
     	// give team a starting balance of 6
-    	balances[_walletAddress] = 6; // _walletAddress CAN BE OVERWRITTEN WITH NO CHECKS
+    	balances[_walletAddress] = 6; // 5. _walletAddress CAN BE OVERWRITTEN WITH NO CHECKS
     	TeamRegistered(_teamName);
     }
 
@@ -74,7 +77,8 @@ contract Lottery{
 
 
     // make your guess , return a success flag
-    function makeAGuess(address _team,uint256 _guess) external payable  returns (bool){ // _team IS NOT CHECKED, ANYONE CAN GUESS FOR ANYONE ELSE
+    function makeAGuess(address _team, uint256 _guess) external payable  returns (bool){
+    	// 6. _team IS NOT CHECKED, ANYONE CAN GUESS FOR ANYONE ELSE
 
     	if (checkThatPaid()==false){
     		return false;
@@ -84,13 +88,13 @@ contract Lottery{
     	uint256 random = oracle.getRandomNumber();
     	if(random==_guess){
     		// add 100 points to team score
-    		balances[_team] =+ 100; // =+ BALANCES ALWAYS CHANGED TO 100 (SHOULD BE +=)
+    		balances[_team] =+ 100; // 7. BALANCES ALWAYS CHANGED TO 100 (SHOULD BE +=)
     		TeamCorrectGuess(teamNames[_team]);
             return true;
     	}
     	else{
     	    // wrong answer  - subtract 3 points
-    		balances[_team]-=3; // NO UNDERFLOW CHECK
+    		balances[_team]-=3; // 8. NO UNDERFLOW CHECK
     		return false;
     	}
     }
@@ -100,9 +104,10 @@ contract Lottery{
 
      if(balances[teams[_teamNumber]]>0){
         // send every winning team some ether
-        bool sent =  teams[_teamNumber].send(balances[teams[_teamNumber]]); // SEND FUNCTION CONDITION NOT CHECKED AND USED
+        bool sent =  teams[_teamNumber].send(balances[teams[_teamNumber]]);
+	// 9. SEND FUNCTION CONDITION NOT CHECKED AND USED
         // reset balance
-        balances[teams[_teamNumber]] = 0; // SHOULD ONLY BE USED IF SEND FUNCTION IS TRUE
+        balances[teams[_teamNumber]] = 0; // 9. SHOULD ONLY BE USED IF SEND FUNCTION IS TRUE
         return sent;
      }
 
@@ -128,7 +133,7 @@ contract Lottery{
     // catch any ether sent to the contract
     function() payable {
     	balances[msg.sender] += msg.value;
-    	AddressPaid(msg.sender,msg.value); // EVENT WILL PROBABLY NOT RUN AS GAS WILL RUN OUT
+    	AddressPaid(msg.sender,msg.value); // 10. EVENT WILL PROBABLY NOT RUN AS GAS WILL RUN OUT IN STANDARD TX
      }
 
 
